46faf8f78f0aebeaf7a3bdd7793e5abd2dea54bb
Evsikoff <gsmevsikov@gmail.com>
2025-11-17 16:51:00 +0300

    Fix low-res selection without breaking loading

--- index.5955834b.js
+++ index.5955834b.js
@@ -3,3 +3,10 @@
         var e = Math.min(1.5, browser.devicePixelRatio) || 1;
         return Math.min(window.innerWidth || window.screen.width || 0, window.innerHeight || window.screen.height || 0) <= 768 ? Math.min(1, e) : e
     })()),
+    _defineProperty2(this, "QUALITY_PATH", function() {
+        var e = Math.min(window.innerWidth || window.screen.width || 0, window.innerHeight || window.screen.height || 0) <= 768 ? "lowres" : "hires";
+        return browser.isMobile && "hires" === e && (e = "lowres"),
+        "".concat(e, "/")
+    }()),
+    _defineProperty2(this, "isHiresSetting", 0 === settings.QUALITY_PATH.indexOf("hires")),
+  , qualityPath$2 = "".concat(settings.QUALITY_PATH)
